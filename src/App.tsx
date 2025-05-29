e89108ce7e1f3e40f80668208caca3174f716ef6
Saulo Figueiredo <saulo.fgf21@gmail.com>
2025-05-28 21:52:18 -0300

    NotFound e template

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,10 @@
+import Home from './pages/Home';
+
 import './styles/thema.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task'
-              id='meus'
-              type='number'
-              placeholder='dITAGA'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
